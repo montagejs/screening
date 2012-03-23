c1b45bb53e52e3bc91a3e4396af90b728bdeebdb
Eliseo Soto <eliseo.soto@motorola.com>
2012-03-22 17:27:07 -0700

    Create thead from column names

--- public/control-room/ui/rest-grid.reel/rest-grid.js
+++ public/control-room/ui/rest-grid.reel/rest-grid.js
@@ -7,4 +7,43 @@ var Montage = require("montage/core/core").Montage,
     Component = require("montage/ui/component").Component;
 
 exports.RestGrid = Montage.create(Component, {
+    _dataTable: { value: null },
+    dataTable: {
+        get: function() {
+            return this._dataTable;
+        },
+        set: function(dataTable) {
+            this._dataTable = dataTable;
+        }
+    },
+
+    _columns: { value: null },
+    columns: {
+        get: function() {
+            return this._columns;
+        },
+        set: function(columns) {
+            this._columns = columns;
+        }
+    },
+
+    prepareForDraw: {
+        value: function() {
+            var self = this;
+            var t = self.dataTable;
+
+            // Create thead from column names
+            var head = document.createElement("thead");
+            var headRow = document.createElement("tr");
+            self.columns.forEach(function(column) {
+                var headColumn = document.createElement("th");
+                var headColumnText = document.createTextNode(column);
+                headColumn.appendChild(headColumnText);
+                headRow.appendChild(headColumn);
+            });
+
+            head.appendChild(headRow);
+            t.appendChild(head);
+        }
+    }
 });
