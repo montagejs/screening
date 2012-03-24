6a229d988544c3bce3fead1ac1a78af14a2a620d
Eliseo Soto <eliseo.soto@motorola.com>
2012-03-23 17:24:18 -0700

    Create a Repetition object manually

--- public/control-room/ui/rest-grid.reel/rest-grid.js
+++ public/control-room/ui/rest-grid.reel/rest-grid.js
@@ -4,7 +4,8 @@
  (c) Copyright 2011 Motorola Mobility, Inc.  All Rights Reserved.
  </copyright> */
 var Montage = require("montage/core/core").Montage,
-    Component = require("montage/ui/component").Component;
+    Component = require("montage/ui/component").Component,
+    Repetition = require("montage/ui/repetition.reel").Repetition;
 
 exports.RestGrid = Montage.create(Component, {
     _dataTable: { value: null },
@@ -27,7 +28,32 @@ exports.RestGrid = Montage.create(Component, {
         }
     },
 
-    prepareForDraw: {
+    _restResourceUrl: { value: null },
+    restResourceUrl: {
+        get: function() {
+            return this._restResourceUrl;
+        },
+        set: function(restResourceUrl) {
+            this._restResourceUrl = restResourceUrl;
+        }
+    },
+
+    templateDidLoad: {
+        value: function() {
+            var self = this;
+
+            // Repetition element
+            var repetitionElement = document.createElement("tbody");
+
+            self._repetition = Montage.create(Repetition, {
+                element: {
+                    value: repetitionElement
+                }
+            });
+        }
+    },
+
+    draw: {
         value: function() {
             var self = this;
             var t = self.dataTable;
@@ -35,14 +61,29 @@ exports.RestGrid = Montage.create(Component, {
             // Create thead from column names
             var head = document.createElement("thead");
             var headRow = document.createElement("tr");
-            self.columns.forEach(function(column) {
+            self._columns.forEach(function(column) {
                 var headColumn = document.createElement("th");
                 var headColumnText = document.createTextNode(column);
                 headColumn.appendChild(headColumnText);
                 headRow.appendChild(headColumn);
             });
-
             head.appendChild(headRow);
+
+            // Get the data from restResourceUrl
+            var xhr = new XMLHttpRequest();
+
+            xhr.onload = function(event) {
+                // Parse the response
+                var data = JSON.parse(event.target.responseText);
+
+                console.log(data);
+            };
+
+            var url = self._restResourceUrl + '&limit=3';
+            xhr.open("GET", url);
+            xhr.send();
+
+            // append the remaining parts to the main table
             t.appendChild(head);
         }
     }
